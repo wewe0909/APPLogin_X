8c8fe360c728a2e3807f1db035b264a2704bb9e7
wewe0909 <solstarx3@gmail.com>
2025-01-28 20:03:15 +0100

    fiallos: changepwd

---
+++ frontend/php/changepwd.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PASSWORD CHANGE</title>
+    <link rel="stylesheet" href="../css/start.css">
+</head>
+<body>
+    <fieldset class="container">
+        <h1>APP Login Example: Password Change</h1>
+        <!--<fieldset id="server_msg">Usuario no existe lol.</fieldset>-->
+        <fieldset class="p1"> 
+            <form id="changepwd">
+                <label>Current username:</label><br>
+                <input type="text"><br>
+                <label>Write your desired password:</label><br>
+                <input type="text"><br>
+                <input type="submit" value="Modify">
+                <!-- <p>You'll receive an email with a code to validate this change.</p> -->
+            </form>
+        </fieldset>
+    </fieldset>
+
+
+    <!-- <div class="alignimage">
+        <img  src="../src/validemail.png" height=450px>
+    </div> -->
+</body>
+</html>
